02d155b787f7371b72c178c802102fc5b2dd11f1
davidwingqvist <dw123gorde@gmail.com>
2024-02-19 23:57:33 +0900

    Start of Portfolio Site

---
+++ src/code/Block.js
@@ -0,0 +1,32 @@
+import './Block.css'
+
+function Block()
+{
+    return (
+        <div className='HomePage'>
+            <table className='Block'>
+                <div>
+                    <text>One</text>
+                    <text>Two</text>
+                </div>
+                <text>Another!</text>
+                <text>Test!</text>
+                <text>Test!</text>
+            </table>
+            <table className='Block'>
+                <text>Test!</text>
+                <text>Another!</text>
+                <text>Test!</text>
+                <text>Test!</text>
+            </table>
+            <table className='Block'>
+                <text>Test!</text>
+                <text>Another!</text>
+                <text>Test!</text>
+                <text>Test!</text>
+            </table>
+        </div>
+    );
+}
+
+export default Block;
