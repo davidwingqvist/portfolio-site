5b47dac58b243a90353d3c9d021942966cb94dbe
davidwingqvist <dw123gorde@gmail.com>
2024-02-23 23:53:42 +0900

    added some animations to text

--- src/code/pages/ProjectsPage.js
+++ src/code/pages/ProjectsPage.js
@@ -29,27 +29,27 @@ function ProjectsPage()
                 <Carousel>
                     <CarouselSlide>
                         <a href='https://youtu.be/GrEWl8npL9Y?si=8TJqPfQM_RXs2Ukp' target='_blank' rel='noreferrer'><img src={images[0]} className='CarouselPicture'></img></a>
-                        <h2>HomeHearth</h2>
+                        <h2 className='FancyText'>HomeHearth</h2>
                     </CarouselSlide>
                     <CarouselSlide>
                         <a href='https://www.youtube.com/watch?v=rBCvf188pP0' target='_blank' rel='noreferrer'><img src={images[1]} className='CarouselPicture'></img></a>
-                        <h2>Prodigium</h2>
+                        <h2 className='FancyText'>Prodigium</h2>
                     </CarouselSlide>
                     <CarouselSlide>
                         <a href='https://github.com/davidwingqvist/Skriptning_DV1570_Grupp5' target='_blank' rel='noreferrer'><img src={images[2]} className='CarouselPicture'></img></a>
-                        <h2>Budget Isaac</h2>
+                        <h2 className='FancyText'>Budget Isaac</h2>
                     </CarouselSlide>
                     <CarouselSlide>
                         <a href='https://www.indiedb.com/games/mortem-the-game' target='_blank' rel='noreferrer'><img src={images[3]} className='CarouselPicture'></img></a>
-                        <h2>Mortem</h2>
+                        <h2 className='FancyText'>Mortem</h2>
                     </CarouselSlide>
                     <CarouselSlide>
                         <a href='https://www.indiedb.com/games/the-wild-pixels' target='_blank' rel='noreferrer'><img src={images[4]} className='CarouselPicture'></img></a>
-                        <h2>Wild Pixels</h2>
+                        <h2 className='FancyText'>Wild Pixels</h2>
                     </CarouselSlide>
                     <CarouselSlide>
                         <a href='https://www.youtube.com/watch?v=pinMUjK6D8E' target='_blank' rel='noreferrer'><img src={images[8]} className='CarouselPicture'></img></a>
-                        <h2>Unity Game</h2>
+                        <h2 className='FancyText'>Unity Game</h2>
                     </CarouselSlide>
                 </Carousel>
             </div>
@@ -58,15 +58,15 @@ function ProjectsPage()
                 <Carousel>
                     <CarouselSlide>
                         <a href='https://davidwingqvist.github.io/Calculator-Website/' target='_blank' rel='noreferrer'><img src={images[7]} className='CarouselPicture'></img></a>
-                        <h2>電卓サイト/Calculator Website</h2>
+                        <h2 className='FancyText'>電卓サイト/Calculator Website</h2>
                     </CarouselSlide>
                     <CarouselSlide>
                         <a href='https://github.com/davidwingqvist/portfolio-site' target='_blank' rel='noreferrer'><img src={images[9]} className='CarouselPicture'></img></a>
-                        <h2>当ポートフォリオサイト</h2>
+                        <h2 className='FancyText'>当ポートフォリオサイト</h2>
                     </CarouselSlide>
                     <CarouselSlide>
                         <a href='https://github.com/davidwingqvist/four-in-a-row' target='_blank' rel='noreferrer'><img src={images[10]} className='CarouselPicture'></img></a>
-                        <h2>簡単なWebゲーム</h2>
+                        <h2 className='FancyText'>簡単なWebゲーム</h2>
                     </CarouselSlide>
                 </Carousel>
             </div>
@@ -75,11 +75,11 @@ function ProjectsPage()
                 <Carousel>
                     <CarouselSlide>
                         <a href='https://github.com/davidwingqvist/HearthEngine' target='_blank' rel='noreferrer'><img src={images[5]} className='CarouselPicture'></img></a>
-                        <h2>ゲームエンジンHearthEngine</h2>
+                        <h2 className='FancyText'>ゲームエンジンHearthEngine</h2>
                     </CarouselSlide>
                     <CarouselSlide>
                         <a href='https://github.com/davidwingqvist/RECS' target='_blank' rel='noreferrer'><img src={images[6]} className='CarouselPicture'></img></a>
-                        <h2>データ指向型RECSデータ管理システム</h2>
+                        <h2 className='FancyText'>データ指向型RECSデータ管理システム</h2>
                     </CarouselSlide>
                 </Carousel>
             </div>
