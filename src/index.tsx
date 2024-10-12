ac7468adae301107664ed3a1ba7910f3d85988b4
davidwingqvist <dw123gorde@gmail.com>
2024-10-12 12:42:40 +0900

    reworked portfolio site, making it new from the ground up

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Block from './code/Block';
-import {
-  BrowserRouter as Router,
-} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
-  <Router>
-    <Block/>
-  </Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,12 @@
+import './Homepage.css'
+
+function HomePage ()
+{
+    return (<div className="main-body">
+        <div className="under-construction-text">
+        構築中
+        </div>
+    </div>);
+}
+
+export default HomePage;
